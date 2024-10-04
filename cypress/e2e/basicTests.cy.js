c602163346544f2162bce13bbb4d6e6c05e13ec6
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-10-04 22:27:25 +0200

    some fixes

--- cypress/e2e/basicTests.cy.js
+++ cypress/e2e/basicTests.cy.js
@@ -3,9 +3,9 @@ describe("tests", () => {
   it("should open and close a modal", () => {
     cy.visit("http://localhost:5173/");
     cy.contains("Open The Modal").click();
+    cy.get(".modal-overlay").should("exist");
     cy.contains("Close").click();
     cy.get(".modal-overlay").should("not.exist");
-    cy.get(".modal").should("not.exist");
   });
 
   it("should fetch a list from API", () => {
@@ -23,7 +23,7 @@ describe("tests", () => {
     cy.visit("http://localhost:5173/");
 
     //Wait for the intercepted request
-    /* cy.wait("@getProducts"); Fix a problem */
+    cy.wait("@getProducts"); //Fix a problem
 
     // Verify the content on the page
     cy.get("tbody tr").should("have.length", 3);
